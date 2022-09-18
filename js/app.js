b14a9d109bbd6919359b575f119ffa030364363d
codeboyfriend <mkinggrafix@gmail.com>
2022-09-18 04:02:48 -0700

    Make Changes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const musicContainer = document.querySelector('.music-container');
 const toggleBtn = document.getElementById('toggle-btn');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
+const prevBtnSec = document.getElementById('prev30s');
+const nextBtnSec = document.getElementById('next30s');
 const nextBtn = document.getElementById('next');
 const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
@@ -9,18 +11,27 @@ const title = document.getElementById('title');
 const audio = document.getElementById('audio');
 const songCurrentTime = document.getElementById('currentTime');
 const songDuration = document.getElementById('duration');
+const fastBtn = document.querySelector('.fast_btn');
 // const cover = document.getElementById('cover');
 
 // DarkMode Function
 const toggleMode = () => {
     document.body.classList.toggle('dark-theme');
+
+    if(document.body.classList.contains('dark-theme')){
+        toggleBtn.querySelector('.fa-solid').classList.remove('fa-sun');
+        toggleBtn.querySelector('.fa-solid').classList.add('fa-moon');
+    }else {
+        toggleBtn.querySelector('.fa-solid').classList.add('fa-sun');
+        toggleBtn.querySelector('.fa-solid').classList.remove('fa-moon');
+    }
 }
 
 toggleBtn.addEventListener('click', toggleMode);
 
 
 // Song titles
-let songs = ['hey', 'hello', 'world', 'love', 'hate'];
+let songs = ['hey', 'hello', 'world', 'love', 'hate', 'glitter'];
 
 // keep track of songs
 let songIndex = 2;
@@ -37,11 +48,17 @@ function loadSong(song) {
 
 function playSong() {
     musicContainer.classList.add('play');
+    playBtn.querySelector('.fa-solid').classList.add('fa-pause')
+    playBtn.querySelector('.fa-solid').classList.remove('fa-play')
+
     audio.play();
 }
 
 function pauseSong() {
     musicContainer.classList.remove('play');
+    playBtn.querySelector('.fa-solid').classList.remove('fa-pause')
+    playBtn.querySelector('.fa-solid').classList.add('fa-play')
+
     audio.pause();
 }
 
@@ -75,33 +92,58 @@ const updateProgress = (e) => {
     progress.style.width = `${progressPercent}%`
 };
 
+const prevSec = () => {
+    audio.currentTime = audio.currentTime - 30.000
+}
+
+const nextSec = () => {
+    audio.currentTime = audio.currentTime + 30.000
+}
+
+const fastForward = () => {
+    let test = (audio.duration / 3 )
+    audio.duration = test
+    // console.log(test)
+}
 
 const updateDuration = (e) => {
     let duration = 0;
     duration = e.srcElement.duration;
-    let isDurationHour = Math.floor(duration / 3600);
     let isDurationMin = Math.floor(duration / 60);
+    let isDurationSec= duration.toString().split(".")[1].substring(0, 2);
 
-    songDuration.innerText = `${isDurationHour}:${isDurationMin}`;
+    isDurationMin = (isDurationMin >= 10) ? isDurationMin : `0${isDurationMin}`;
+    isDurationSec = (isDurationSec >= 10) ? isDurationSec : `0${isDurationSec}`;
+    
+    songDuration.innerText = `${isDurationMin}:${isDurationSec}`;
 }
 
+// variable declaration
+let time = 0
+let sec = 0;
+let min = 0;
+
 const updateCurrentTime = (e) => {
-    let sec = 0;
-    let min = 0;
     let { currentTime } = e.srcElement;
     let roundCurrentTime = Math.floor(currentTime);
-    // console.log(roundCurrentTime)
+    
+    time++;
 
-    if( roundCurrentTime % 2 === 0 ) {
+    if (time % 4 === 0) {
         sec = sec + 1
-        console.log(sec)
     }
 
-    // if (roundCurrentTime  % 60 == 0) {
-    //     min = min + 1;
-    // }
+    if(time % 240 === 0){
+        min = min + 1
+    }
 
-    // songCurrentTime.innerText = `${min}:${roundCurrentTime}`
+    if ( sec % 60 === 0) {
+        sec = 0
+    }
+
+    
+
+    songCurrentTime.innerText = `${min}:${sec}`
     // console.log(roundCurrentTime)
     
 }
@@ -128,9 +170,11 @@ playBtn.addEventListener('click', () => {
 // Change Song event
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-
+prevBtnSec.addEventListener('click', prevSec);
+nextBtnSec.addEventListener('click', nextSec);
 audio.addEventListener('timeupdate', updateProgress);
 audio.addEventListener('ended', nextSong);
 audio.addEventListener('timeupdate', updateDuration);
 audio.addEventListener('timeupdate', updateCurrentTime)
 progressContainer.addEventListener('click', setProgress);
+fastBtn.addEventListener('click', fastForward);
