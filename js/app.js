272197c9d847fb5bc84ea3b84f9fd64154088ffb
codeboyfriend <mkinggrafix@gmail.com>
2022-09-19 13:28:39 -0700

    Add Changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
 const musicContainer = document.querySelector('.music-container');
+const listContainer = document.querySelector('.list-container');
 const toggleBtn = document.getElementById('toggle-btn');
 const playBtn = document.getElementById('play');
+const playBtn_snippet = document.getElementById('play-snippet');
 const prevBtn = document.getElementById('prev');
 const prevBtnSec = document.getElementById('prev30s');
 const nextBtnSec = document.getElementById('next30s');
 const nextBtn = document.getElementById('next');
 const progress = document.querySelector('.progress');
+const progress_snippet = document.querySelector('.progress-snippet');
 const progressContainer = document.querySelector('.progress-container');
+const progressContainerSnippet = document.querySelector('.progress-container-snippet');
 const title = document.getElementById('title');
+const title_snippet = document.getElementById('title-snippet');
 const audio = document.getElementById('audio');
 const songCurrentTime = document.getElementById('currentTime');
 const songDuration = document.getElementById('duration');
 const fastBtn = document.querySelector('.fast_btn');
-// const cover = document.getElementById('cover');
+const cover = document.getElementById('cover');
+const cover_snippet = document.getElementById('cover-snippet');
+const listItem = document.getElementById('list-item');
+const list = document.getElementById('list');
+const artist = document.getElementById('artist');
+const artist_snippet = document.getElementById('artist-snippet');
+let small = document.getElementById('small');
+const closeSlide = document.getElementById('close-btn');
+const openSlide = document.getElementById('open-btn');
+const minimize = document.querySelector('.minimize_container_box');
+const songInfo = document.querySelector('.song-info');
 
 // DarkMode Function
 const toggleMode = () => {
@@ -27,37 +42,160 @@ const toggleMode = () => {
     }
 }
 
+//  Close Home Function
+const closeSlides = () => {
+    musicContainer.classList.remove('hide');
+    listContainer.classList.remove('hide');
+}
+
+//  Open Home Function
+const openSlides = () => {
+    musicContainer.classList.add('hide');
+    listContainer.classList.add('hide');
+}
+
+// Add Event Listener
 toggleBtn.addEventListener('click', toggleMode);
+closeSlide.addEventListener('click', closeSlides);
+openSlide.addEventListener('click', openSlides);
+minimize.addEventListener('click', openSlides);
 
 
-// Song titles
-let songs = ['hey', 'hello', 'world', 'love', 'hate', 'glitter'];
+// Songs
+let songs = [
+    {
+        id: 0,
+        title: 'hey',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 1,
+        title: 'hello',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 2,
+        title: 'world',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 3,
+        title: 'love',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 4,
+        title: 'hate',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 5,
+        title: 'glitter',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 6,
+        title: 'Boy',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 7,
+        title: 'girl',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 8,
+        title: 'empty',
+        artist: 'codeboyfriend'
+    },
+    {
+        id: 9,
+        title: 'moonlight',
+        artist: 'codeboyfriend'
+    },
+]
+let songIndexNo = 0;
+
+const listSongs = () => {
+    songs.map((song) => {
+        // Create DOM ELement
+        let div = document.createElement('div')
+        let li = document.createElement('li');
+        let p = document.createElement('p');
+
+        // Add Classname
+        div.className = 'song-info';
+        li.className = 'music-list';
+        p.className = 'artist';
+
+        // AppendChild
+        div.appendChild(li)
+        div.appendChild(p)
+        li.appendChild(document.createTextNode(song.title));
+        p.appendChild(document.createTextNode(`${song.artist} ${' - '} ${song.title}`));
+
+        // Add EventListener
+        div.addEventListener('click', (id) => {
+            songIndex = song.id;
+            loadSong(songs[songIndex]);
+            playSong();
+            musicContainer.classList.add('hide');
+            listContainer.classList.add('hide');
+
+            if (song.id === songIndex) {
+                div.classList.add('isPlaying')
+                div.style.backgroundColor = 'red'
+            }
+
+            console.log(songIndex)
+            console.log(song.id)
+        })
+        
+        // Append to ul
+        list.appendChild(div)
+        
+        // Append to the Dom
+        listItem.appendChild(list)
+    })
+}
+
+// Call listSongs function
+listSongs()
 
 // keep track of songs
-let songIndex = 2;
+let songIndex = songIndexNo;
 
 // Initially load song info DOM
 loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    // cover.src = `images/${song}.jpg`;
+    small.innerText = songs.length;
+    title.innerText = song.title;
+    title_snippet.innerText = song.title;
+    artist.innerText = song.artist;
+    artist_snippet.innerText = song.artist;
+    audio.src = `music/${song.title}.mp3`;
+    cover.src = `images/${song.title}.jpg`;
+    cover_snippet.src = `images/${song.title}.jpg`;
 }
 
 function playSong() {
     musicContainer.classList.add('play');
-    playBtn.querySelector('.fa-solid').classList.add('fa-pause')
-    playBtn.querySelector('.fa-solid').classList.remove('fa-play')
+    playBtn.querySelector('.fa-solid').classList.add('fa-pause');
+    playBtn.querySelector('.fa-solid').classList.remove('fa-play');
+    playBtn_snippet.querySelector('.fa-solid').classList.add('fa-pause');
+    playBtn_snippet.querySelector('.fa-solid').classList.remove('fa-play');
 
     audio.play();
 }
 
 function pauseSong() {
     musicContainer.classList.remove('play');
-    playBtn.querySelector('.fa-solid').classList.remove('fa-pause')
-    playBtn.querySelector('.fa-solid').classList.add('fa-play')
+    playBtn.querySelector('.fa-solid').classList.remove('fa-pause');
+    playBtn.querySelector('.fa-solid').classList.add('fa-play');
+    playBtn_snippet.querySelector('.fa-solid').classList.remove('fa-pause');
+    playBtn_snippet.querySelector('.fa-solid').classList.add('fa-play');
 
     audio.pause();
 }
@@ -66,7 +204,7 @@ const prevSong = () => {
     songIndex = songIndex - 1;
 
     if (songIndex < 0) {
-        songs = songs.length - 1;
+        songIndex = songs.length - 1;
     }
 
     loadSong(songs[songIndex]);
@@ -89,7 +227,8 @@ const nextSong = () => {
 const updateProgress = (e) => {
     const { duration, currentTime } = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`
+    progress.style.width = `${progressPercent}%`;
+    progress_snippet.style.width = `${progressPercent}%`;
 };
 
 const prevSec = () => {
@@ -103,7 +242,6 @@ const nextSec = () => {
 const fastForward = () => {
     let test = (audio.duration / 3 )
     audio.duration = test
-    // console.log(test)
 }
 
 const updateDuration = (e) => {
@@ -148,8 +286,17 @@ const updateCurrentTime = (e) => {
     
 }
 
+
 const setProgress = (e) => {
     const width = progressContainer.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+const setSnippetProgress = (e) => {
+    const width = progressContainerSnippet.clientWidth;
     const clickX = e.offsetX;
     const duration = audio.duration;
 
@@ -177,4 +324,5 @@ audio.addEventListener('ended', nextSong);
 audio.addEventListener('timeupdate', updateDuration);
 audio.addEventListener('timeupdate', updateCurrentTime)
 progressContainer.addEventListener('click', setProgress);
+progressContainerSnippet.addEventListener('click', setSnippetProgress);
 fastBtn.addEventListener('click', fastForward);
